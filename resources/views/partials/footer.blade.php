b4037569e1baefcc32603df6d4006d6de9e8a5cd
Jason Ma <jason@madebyon.com>
2021-03-07 02:32:35 +0800

    Service ACF

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -12,7 +12,7 @@
       </div>
 
       <div class="col-sm-6">
-      <?php echo do_shortcode( '[contact-form-7 id="3585" title="Contact form 1"]' ); ?>
+        <?php echo do_shortcode( '[contact-form-7 id="3585" title="Contact form 1"]' ); ?>
       </div>
     </div>
 
