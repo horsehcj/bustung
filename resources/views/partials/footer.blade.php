3028822f3337b3fe8c469cbae041fb1e25fc9ce9
Jason Ma <jason@madebyon.com>
2021-03-15 13:44:26 +0800

    Home page responsiveness

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,23 +1,23 @@
 <footer class="footer-info">
   <div class="container">
     <div class="row justify-content-center">
-      <div class="col-lg-10">
+      <div class="col-12">
         <img class="footer-logo" src="@asset('images/bustung_logo.png')">
       </div>
 
-      <div id="contact" class="col-md-6 col-lg-5">
+      <div id="contact" class="col-md-6">
         <p>電話：<a href="tel:2330 2990">2330 2990</a></p>
         <p>電話：<a href="https://api.whatsapp.com/send?phone=94984445">9498 4445</a> (Whatsapp)</p>
         <p>地址：九龍紅磡馬頭圍道37號紅磡商業中心B座5樓508室</p>
       </div>
 
-      <div class="col-md-6 col-lg-5">
+      <div class="col-md-6">
         <?php echo do_shortcode( '[contact-form-7 id="3585" title="Contact form 1"]' ); ?>
       </div>
     </div>
 
     <div class="row justify-content-center">
-      <div class="col-lg-10">
+      <div class="col-12">
         <p class="copyright">© 2021 BusTung Limited 巴士通有限公司。 版權所有</p>
       </div>
     </div>
